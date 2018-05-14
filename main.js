196d3abcbf7252e11c6bd7680bdecbf638198044
kingnowdo <reinaldomagas@gmail.com>
2018-05-14 15:06:52 -0300

    Um programa diferente...

--- main.js
+++ main.js
@@ -32,3 +32,18 @@ function palindromo(){
     }
     if(tex==user) console.log("sim, é um palíndromo")
 }
+
+function diferente(){
+    for(var i=1;i<=100;i=i+1){
+        if (i%3==0){
+            if(i%5==0) console.log('FizzBuzz');
+            else console.log('Fizz');
+        }
+        else if(i%5==0){
+            console.log('Buzz');
+        }
+        else{
+            console.log(i);
+        }
+    }
+}
